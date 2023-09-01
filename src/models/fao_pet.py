d76eeb5bb8cd697813358ac15cfd17f1ad9dc313
RY4GIT <400mhs2@gmail.com>
2023-08-31 22:53:09 -0700

    Completed adding PET module

--- src/models/fao_pet.py
+++ src/models/fao_pet.py
@@ -9,7 +9,7 @@ import math
 log = logging.getLogger("models.dCFE")
 # 
 # https://eto.readthedocs.io/en/latest/package_references.html
-# Only few user exsits, possibly swtich to https://github.com/pyet-org/pyet or https://github.com/woodcrafty/PyETo
+# Only few user exsits, possibly swtich to https://github.com/pyet-org/pyet (only daily) or https://github.com/woodcrafty/PyETo
 
 class FAO_PET():
     
@@ -25,18 +25,23 @@ class FAO_PET():
 
         # Get CAMELS basin attributes
         basin_attrs = pd.read_csv(self.cfg.camels_attr_file)
-        basin_idx = basin_attrs['basin_id']==self.cfg.data.basin_id
-        self.elevation = basin_attrs['elevation'][basin_idx]
-        self.zw = 10.0 # basin_attrs['zw'][basin_idx] #TODO: check what is this
-        self.lon = basin_attrs['lon'][basin_idx]
-        self.lat = basin_attrs['lat'][basin_idx]
+        basin_attrs['gauge_id'] = basin_attrs['gauge_id'].astype(str).str.zfill(8)
+        basin_idx = basin_attrs['gauge_id']==self.cfg.data.basin_id
+        self.lon = basin_attrs['gauge_lon'][basin_idx].values[0]
+        self.lat = basin_attrs['gauge_lat'][basin_idx].values[0]
+        self.elevation = basin_attrs['elev_mean'][basin_idx].values[0]
 
         
     def calc_PET(self):
         
-        PET = ETo(self.new_df, 'H', z_msl=self.elevation, lat=self.lat, lon=self.lon, TZ_lon=self.lon).eto_fao() 
+        PET = ETo(self.input_forcing, freq='H', lon=self.lon, TZ_lon=self.lon, z_msl=self.elevation, lat=self.lat).eto_fao() 
+        # Don't need lat: (only needed if R_s or R_n are not in df)
+        # Don't need z_msl ((nly needed if P is not in df)
+        # lon: The longitude of the met station (dec deg) (only needed if calculating ETo hourly)
+        # TZ_lon: The longitude of the center of the time zone (dec deg) (only needed if calculating ETo hourly).
+        # freq (str) – The Pandas time frequency string of the input and output. The minimum frequency is hours (H) and the maximum is month (M).
         PET = PET.fillna(0)
-        PET = PET / self.cfg.conversions.m_to_mm  #mm/hr to m/hr  
+        PET = PET / self.cfg.conversions.m_to_mm / self.cfg.conversions.hr_to_sec  #mm/hr to m/hr  to m/s
 
         return PET
     
@@ -44,47 +49,59 @@ class FAO_PET():
 
         # Convert the time column to DateTime type
         df['time'] = pd.to_datetime(df['date'])
+        df['date'] = pd.to_datetime(df['date'])
 
         # Set the time column as the index
         df.set_index('time', inplace=True)
 
         # Calculate relative humidity for each row
-        df["Relative_Humidity"] = df.apply(self.calculate_relative_humidity, axis=1)
-
-        df["Actual_Vapor_Pressure"] = df.apply(self.calculate_actual_vapor_pressure, axis=1)
-
-        df["day_of_year"] = df.index.dayofyear
-
-        df["utc_hour"] = df.index.tz_localize(None).hour
+        df["RH_mean"] = df.apply(self.calculate_relative_humidity, axis=1)
         
-        data = {
-            'R_s': df["shortwave_radiation"] * 0.0036,       
-            'T_mean': df["temperature"] - 273.15, #C
-            'e_a': df["Actual_Vapor_Pressure"] / 1000,  #Kpa
-            'RH_mean': df["Relative_Humidity"],
-            'date': df.index
-        }
+        # Actual vapor pressure
+        df["e_a"] = df.apply(self.calculate_actual_vapor_pressure_Pa, axis=1) / self.cfg.conversions.to_kilo
 
-        input_forcing = pd.DataFrame(data)
-        input_forcing.index = pd.to_datetime(input_forcing.index)
+        # Mean find speed
+        df['U_z']= (df["wind_u"] + df["wind_v"])/2
+        
+        # Unit conversion
+        df['R_s'] = df["shortwave_radiation"] * 0.0036 # self.cfg.conversions.day_to_sec / self.cfg.conversions.to_mega
+        df['P'] = df["pressure"] / self.cfg.conversions.to_kilo
+        df['T_mean'] = df["temperature"]
+
+        input_forcing = df[["date", "R_s", "P", "T_mean", "e_a", "RH_mean", "U_z"]]
+
+        # {
+        #     'date': df.index,
+        #     'R_s': df["shortwave_radiation"] * self.cfg.conversions.day_to_sec / self.cfg.conversions.to_mega,     # (W/m2) -> (MJ/m2/day)  
+        #     'P': df["pressure"] / self.cfg.conversions.to_kilo, # (Pa) -> (kPa)
+        #     'T_mean': df["temperature"], # (deg C) -> (deg C)
+        #     'e_a': df["Actual_Vapor_Pressure"] / self.cfg.conversions.to_kilo,  # (Pa) -> kPa? 
+        #     'RH_mean': df["Relative_Humidity"], # (-)
+        #     'U_z': df["mean_wind_speed"], # (m/s) -> (m/s)
+        # }
         
         return input_forcing
 
-    
-    def calculate_relative_humidity(self, row):
-        specific_humidity = row["specific_humidity"]
-        temperature = row["temperature"] - 273.15
-        pressure = row["pressure"]/100
-        p_sat = 6.112 * math.exp((17.67 * temperature) / (temperature + 243.5))
-        relative_humidity = (specific_humidity / (1 - specific_humidity)) * (p_sat / pressure) * 100
-        return relative_humidity    
-    
-    
-    def calculate_actual_vapor_pressure(self, row):
-        temperature = row["temperature"] - 273.15  # Temperature in Celsius
-        specific_humidity = row["specific_humidity"]  # Specific humidity
-        pressure = row["pressure"]  # Atmospheric pressure in pascals
+    # References: 
+    # https://cran.r-project.org/web/packages/humidity/vignettes/humidity-measures.html#eq:1
+    # https://www.fao.org/3/x0490e/x0490e07.htm
+    # https://github.com/NOAA-OWP/evapotranspiration/blob/1e971ffe784ade3c7ab322ccce6f49f48e942e3d/src/pet.c
 
-        actual_vapor_pressure = specific_humidity * pressure / (0.622 + 0.378 * specific_humidity)
-        return actual_vapor_pressure    
+    def calculate_relative_humidity(self, row):
+        air_sat_vap_press_Pa = self.calc_air_saturation_vapor_pressure_Pa(row)
+        actual_vapor_pressure_Pa = self.calculate_actual_vapor_pressure_Pa(row)
+        relative_humidity = actual_vapor_pressure_Pa / air_sat_vap_press_Pa
+        return relative_humidity
+    
+    def calc_air_saturation_vapor_pressure_Pa(self, row):
+        air_temperature_C = row["temperature"]
+        air_sat_vap_press_Pa = 611.0 * math.exp(17.27 * air_temperature_C / (237.3 + air_temperature_C))
+        return air_sat_vap_press_Pa
+
+    def calculate_actual_vapor_pressure_Pa(self, row):
+        # https://cran.r-project.org/web/packages/humidity/vignettes/humidity-measures.html#eq:1
+        q = row["specific_humidity"]  # Specific humidity
+        p = row["pressure"]  # Atmospheric pressure in pascals
+        actual_vapor_pressure_Pa = q * p / (0.622 + 0.378 * q)
+        return actual_vapor_pressure_Pa    
 
