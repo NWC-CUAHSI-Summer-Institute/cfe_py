fix: normalization. Next is flatten

src/data/Data.py
@@ -59,7 +59,7 @@ class Data(Dataset):
         :param index: the date you're iterating on
         :return: the forcing and observed data for a particular timestep
         """
-        # Retrun the precip and PET tensor at timestep index
+        # Retrun the precip and PET tensor, and observed/synthetic flow at timestep index
         return self.x[:, index, :], self.y[:, index, :]
 
     def __len__(self):
@@ -120,7 +120,7 @@ class Data(Dataset):
                 obs_q_["QObs(mm/h)"][self.start_time : self.end_time].copy().values
                 / cfg.conversions.m_to_mm,
                 device=cfg.device,
-            )  # TODO: Check unit conversion
+            )
             y_ = torch.stack([q])
             y_tr = y_.transpose(0, 1)
             output_tensor[i] = y_tr
@@ -140,7 +140,7 @@ class Data(Dataset):
         self.obs_q = synthetic_q[self.start_time : self.end_time].copy()
         # self.n_timesteps = len(self.obs_q)
 
-        return torch.tensor(self.obs_q.y_hat, device=cfg.device)
+        return torch.tensor(self.obs_q.values, device=cfg.device)
 
     def get_dynamic_attributes(self, cfg: DictConfig):
         output_tensor = torch.zeros([len(self.basin_ids), self.n_timesteps, 3])

src/utils/transform.py
@@ -39,11 +39,11 @@ def normalization(x):
     :return:
     """
     min_max_scaler = preprocessing.MinMaxScaler()
-    x_trans = x.transpose(1, 0)
+    x_trans = x.transpose(2, 0).transpose(2, 1)
     x_tensor = torch.zeros(x_trans.shape)
     for i in range(0, x_trans.shape[0]):
         x_tensor[i, :] = torch.tensor(
-            min_max_scaler.fit_transform(x_trans[i, :].reshape(-1, 1)).transpose(1, 0)
-        )
+            min_max_scaler.fit_transform(x_trans[i, :].transpose(1, 0))
+        ).transpose(1, 0)
     """Transposing to do correct normalization"""
-    return torch.transpose(x_tensor, 1, 0)
+    return x_tensor.transpose(1, 0).transpose(1, 2)